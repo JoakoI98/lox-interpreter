2cf78547bd3db1a0b2c935ecd43f41f8440483f6
Joaco Itria <joaquin.itria@truepill.com>
2025-07-12 01:41:30 -0300

    Implemented eval

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use ast_leaf::ast_leaf;
 //     pub nt: String,
 // }
 
+mod evaluation;
 mod syntax_analysis;
 mod tokenizer;
 
@@ -77,6 +78,45 @@ fn run() {
                 let r = parse_stream.parse::<syntax_analysis::Expression>();
                 match r {
                     Ok(r) => println!("{}", r),
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        std::process::exit(65);
+                    }
+                }
+            } else {
+                println!("EOF  null"); // Placeholder, replace this line when implementing the scanner
+            }
+        }
+        "evaluate" => {
+            // You can use print statements as follows for debugging, they'll be visible when running tests.
+            writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
+
+            let file_contents = fs::read_to_string(filename).unwrap_or_else(|err| {
+                writeln!(io::stderr(), "Failed to read file {}: {}", filename, err).unwrap();
+                String::new()
+            });
+
+            if !file_contents.is_empty() {
+                let (tokens, errors) = scan_tokens(file_contents.as_str());
+                for error in &errors {
+                    eprintln!("{}", error);
+                }
+                if !errors.is_empty() {
+                    std::process::exit(65);
+                }
+                let mut parse_stream = syntax_analysis::ParseStream::new(tokens);
+                let r = parse_stream.parse::<syntax_analysis::Expression>();
+                match r {
+                    Ok(r) => {
+                        let r = r.eval();
+                        match r {
+                            Ok(r) => println!("{}", r),
+                            Err(e) => {
+                                eprintln!("{}", e);
+                                std::process::exit(65);
+                            }
+                        }
+                    }
                     Err(e) => {
                         eprintln!("{}", e);
                         std::process::exit(65);
